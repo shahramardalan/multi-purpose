4e135d864b32eead914b67d160fccb62ccc6ceec
shahram <ardalan.shahram@gmail.com>
2025-02-02 23:55:38 +0330

    commit 2

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By <a
+            href="https://github.com/shahramardalan">Shahram
+            Ardalan</a>
+    </div>
+    <div class="footer-right">
+
+    </div>
+</footer>
---
+++ resources/views/admin/layouts/sidbar.blade.php
@@ -0,0 +1,22 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="dashboard">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Starter</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Layout</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+                </ul>
+            </li>
+            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank
+                        Page</span></a></li>
+        </ul>
+    </aside>
+</div>
